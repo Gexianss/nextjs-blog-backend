4de429ee1b52a8ba09fa059ecb72e1f578e8ef3a
wei shan <shiaushen@gmail.com>
2024-03-26 14:50:05 +0800

    update notion_token

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ dotenv.config()
 const app = express()
 const port = 3001
 
-const notion = new Client({ auth: "secret_DY3ag7qSv5NQhmWHGIeft6Uka5C0OZByCsIXMhuXJmm" })
+const notion = new Client({ auth: process.env.NOTION_TOKEN })
 
 app.use(cors({ origin: 'http://localhost:3000' }));
 
 app.get('/api/posts', async (req, res) => {
     try {
-        const response = await notion.databases.query({ database_id: "fd67350175054ee7bdc24cb5cb80718b" })
+        const response = await notion.databases.query({ database_id: process.env.NOTION_DATABASE_ID })
         const parsedData = response.results.map(page => ({ id: page.id, data: page.properties }))
         res.json(parsedData)
     } catch (error) {
